aa6b95425b45494f0c528d6b63756c162019cd06
mir-of <mir-of@outlook.com>
2019-03-05 20:31:25 +0800

    add load model param for run_test.sh

--- official/resnet/run_test.sh
+++ official/resnet/run_test.sh
@@ -3,6 +3,7 @@ python imagenet_main.py \
 --loss_scale=1 \
 --num_gpus=1 \
 --batch_size=5 \
---train_epochs=100 \
+--train_epochs=2 \
 --max_train_steps=100 \
---data_dir=/home/qiaojing/tmp/dataset/tf_imagenet_224_5pic
+--data_dir=/home/qiaojing/tmp/dataset/tf_imagenet_224_5pic \
+--pretrained_model_checkpoint_path=/home/qiaojing/tmp/resnet_model/tf_ckpt
