1ff3fa199523acf805040fde0b6073b2fe669855
mir-of <mir-of@outlook.com>
2019-03-06 21:34:34 +0800

    add data_format in run_test.sh

--- official/resnet/run_test.sh
+++ official/resnet/run_test.sh
@@ -1,9 +1,10 @@
 python imagenet_main.py \
 --model_dir=./test_output_dir \
 --loss_scale=1 \
+--data_format='channels_first' \
 --num_gpus=1 \
 --batch_size=5 \
---train_epochs=2 \
---max_train_steps=100 \
+--train_epochs=5 \
+--max_train_steps=5 \
 --data_dir=/home/qiaojing/tmp/dataset/tf_imagenet_224_5pic \
---pretrained_model_checkpoint_path=/home/qiaojing/tmp/resnet_model/tf_ckpt
+--pretrained_model_checkpoint_path=/home/qiaojing/tmp/resnet_model/tf_ckpt 
