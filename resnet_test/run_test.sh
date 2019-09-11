68f99ecd36c7b6d8740a7921124c697a90c2a77c
mir-of <mir-of@outlook.com>
2019-09-11 16:20:58 +0800

    public dir

--- resnet_test/run_test.sh
+++ resnet_test/run_test.sh
@@ -6,8 +6,8 @@ python ../official/resnet/imagenet_main.py \
 --weight_decay=0 \
 --batch_size=1 \
 --train_epochs=100 \
---pretrained_model_checkpoint_path=/home/qiaojing/dev/cnns_test/resnet/models/tf_model \
---data_dir=/home/qiaojing/dev/cnns_test/dataset/PNG228/tf_record 
+--pretrained_model_checkpoint_path=/dataset/PNGS/cnns_model_for_test/resnet50/models/tf_model \
+--data_dir=/dataset/PNGS/PNG228/tf_record 
 
 
 #--pretrained_model_checkpoint_path=/home/qiaojing/tmp/resnet_model/tf_ckpt
